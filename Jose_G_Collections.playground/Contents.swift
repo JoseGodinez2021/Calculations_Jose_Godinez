6859f63342c44666f1bf2d7fc167c97cbb87f253
JoseGodinez2021 <67968966+JoseGodinez2021@users.noreply.github.com>
2020-07-15 11:51:02 -0500

    Jose_g_collections

---
+++ Jose_G_Collections.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var collections : [String] = [ "Call Of Duty: Modern Warfare","Call Of Duty WW2","Bloodborne","Call of Duty: Advanced Warfare","The Witcher 3","No Mans Sky","Tom Clancy's Rainbow Six:Siege", "Tokyo Jungle", "Pokemon: Fire Red", "Minecraft"]
+
+collections += ["Naruto game", "Yakuza 5"]
+
+
+print("My collection has \(collections.count) Video games in it. These video games are:")
+print(collections[0])
+    print(collections[1])
+        print(collections[2])
+            print(collections[3])
+                print(collections[4])
+                    print(collections[5])
+                        print(collections[6])
+print(collections[7])
+print(collections[8])
+ print(collections[9])
+
+
+
+print(collections[10])
+print(collections[11])
+
+
+if collections.isEmpty {
+    print("You need more games dude")
+}    else{
+        print("Do you even have a life? Because I dont")
+}
